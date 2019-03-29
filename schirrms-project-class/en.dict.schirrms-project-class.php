7849e6a1d608f4f5f83ee7bc99fb58a5dcd9f069
Schirrms <schirrms@schirrms.net>
2019-03-29 20:04:15 +0100

    Initial version - 0.1.0

---
+++ schirrms-project-class/en.dict.schirrms-project-class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2019 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:Project' => 'Project',
+	'Class:Project/Attribute:code' => 'Project Code',
+	'Class:Project/Attribute:description' => 'Description',
+	'Class:Project/Attribute:org_id' => 'Organization',
+	'Class:Project/Attribute:defaultVersion' => 'Default Version',
+	'Class:Project/Attribute:coordinator' => 'Coordinator',
+	'Class:Project/Attribute:owner' => 'Project Owner',
+	'Class:Project/Attribute:projectManager' => 'Project Manager',
+	'Class:Project/Attribute:type' => 'Project Type',
+	'Class:Project/Attribute:status' => 'State',
+	'Class:Project/Attribute:status/Value:open' => 'Open',
+	'Class:Project/Attribute:status/Value:closed' => 'Closed',
+	'Class:Project/Attribute:visibility' => 'Visibility',
+	'Class:Project/Attribute:visibility/Value:public' => 'Public',
+	'Class:Project/Attribute:visibility/Value:private' => 'Private',
+	'Class:Project/Attribute:level' => 'Landing',
+	'Class:Project/Attribute:level/Value:true' => 'Yes',
+	'Class:Project/Attribute:level/Value:false' => 'No',
+	'Class:Project/Attribute:subcontractor' => 'Sub Contractor',
+	'Class:Project/Attribute:subcontractor/Value:true' => 'Yes',
+	'Class:Project/Attribute:subcontractor/Value:false' => 'No',
+	'Class:Project/Attribute:domain' => 'Domain(s)',
+	'Class:Project/Attribute:tech' => 'Technology(ies)',
+	'Class:Project/Attribute:functional_cis_list' => 'CI part of the project',
+	'Class:FunctionalCI/Attribute:project_id' => 'Project Code',
+));
+?>
---
+++ schirrms-project-class/fr.dict.schirrms-project-class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2019 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:Project' => 'Projet',
+	'Class:Project/Attribute:code' => 'Code Projet',
+	'Class:Project/Attribute:description' => 'Description',
+	'Class:Project/Attribute:org_id' => 'Organisation',
+	'Class:Project/Attribute:defaultVersion' => 'Version par défaut',
+	'Class:Project/Attribute:coordinator' => 'Coordinateur',
+	'Class:Project/Attribute:owner' => 'MOA',
+	'Class:Project/Attribute:projectManager' => 'MOE',
+	'Class:Project/Attribute:type' => 'Type du projet',
+	'Class:Project/Attribute:status' => 'Etat',
+	'Class:Project/Attribute:status/Value:open' => 'Actif',
+	'Class:Project/Attribute:status/Value:closed' => 'Clos',
+	'Class:Project/Attribute:visibility' => 'Visibilité',
+	'Class:Project/Attribute:visibility/Value:public' => 'Public',
+	'Class:Project/Attribute:visibility/Value:private' => 'Privé',
+	'Class:Project/Attribute:level' => 'Palier',
+	'Class:Project/Attribute:level/Value:true' => 'Oui',
+	'Class:Project/Attribute:level/Value:false' => 'Non',
+	'Class:Project/Attribute:subcontractor' => 'Sous Traitance',
+	'Class:Project/Attribute:subcontractor/Value:true' => 'Oui',
+	'Class:Project/Attribute:subcontractor/Value:false' => 'Non',
+	'Class:Project/Attribute:domain' => 'Domaine(s)',
+	'Class:Project/Attribute:tech' => 'Technologie(s)',
+	'Class:Project/Attribute:functional_cis_list' => 'CI membres du projet',
+	'Class:FunctionalCI/Attribute:project_id' => 'Code Projet',
+));
+?>
